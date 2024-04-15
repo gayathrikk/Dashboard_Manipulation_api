0d46d983fb1e933feef5f5fff76df55126f2f472
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-15 15:13:47 +0530

    ImageQC_api.java

--- src/test/java/com/test/Dashboard_apis/ImageQC_api.java
+++ src/test/java/com/test/Dashboard_apis/ImageQC_api.java
@@ -5,18 +5,40 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class ImageQC_api {
 	
-	@Test(priority=1)
-	public void ImageQC()
-	{
-        Response response1 = RestAssured.get("https://apollo2.humanbrain.in/dashboard/dashboardImageQC");
-        int statusCode1 = response1.getStatusCode();
-        
-        Assert.assertEquals(statusCode1, 200, "API request to the ImageQC failed. Status code: " + statusCode1);
-        
-        System.out.println("API request to the ImageQC passed. Status code: " + statusCode1);
-	}
+private static final Map<String, String> ENDPOINT_LABELS = new LinkedHashMap<>();
+	    static {
+	    	 ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/dashboardImageQC", "Imaging_IN");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/1", "Imaging_ToDo");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/234", "Imaging_Scanning");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/6", "Image_QC");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/11", "Bio_QC");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/7", "Publishing");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/5", "Batch_Failed");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryImageQCBatchs/8", "Image_Out");
+	        
+	    } 
 
-}
+	    @Test
+	    public void testAPIs() {
+	        for (String endpoint : ENDPOINT_LABELS.keySet()) {
+	            String endpointLabel = ENDPOINT_LABELS.get(endpoint);
+	            Response response = RestAssured.get(endpoint);
+
+	            int statusCode = response.getStatusCode();
+	            if (statusCode == 200) {
+	                System.out.println("API request to " + endpointLabel + " passed. Status code: " + statusCode);
+	            } else {
+	                System.out.println("API request to " + endpointLabel + " failed. Status code: " + statusCode);
+	            }
+
+	            Assert.assertEquals(statusCode, 200, "API request to " + endpointLabel + " failed");
+	        }
+	    }}
+
+	
