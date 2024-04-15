79baaeb00ed7fb55091c98caaca43f3c80efafd3
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-15 15:09:14 +0530

    AtlasCreation_api.java

--- src/test/java/com/test/Dashboard_apis/AtlasCreation_api.java
+++ src/test/java/com/test/Dashboard_apis/AtlasCreation_api.java
@@ -5,18 +5,46 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class AtlasCreation_api {
 	
-	@Test(priority=1)
-	public void AtlasCreation()
-	{
-        Response response1 = RestAssured.get("https://apollo2.humanbrain.in/dashboard/AtlasCreation");
-        int statusCode1 = response1.getStatusCode();
+	private static final Map<String, String> ENDPOINT_LABELS = new LinkedHashMap<>();
+    static {
+    	ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreationIn", "Brain_IN");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_brainWait/1", "Brain_Wait");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_SeriesWait_SS", "series_wait");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_SS/12", "IN_Progress");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_SS/3", "Review");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_SS/5", "Reject");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_SS/4", "Out");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/AtlasCreation_Stats", "Stats");
         
-        Assert.assertEquals(statusCode1, 200, "API request to the AtlasCreation failed. Status code: " + statusCode1);
-        
-        System.out.println("API request to the AtlasCreation passed. Status code: " + statusCode1);
-	}
+    } 
 
-}
+    @Test
+    public void testAPIs() {
+        for (String endpoint : ENDPOINT_LABELS.keySet()) {
+            String endpointLabel = ENDPOINT_LABELS.get(endpoint);
+            Response response = RestAssured.get(endpoint);
+
+            int statusCode = response.getStatusCode();
+            if (statusCode == 200) {
+                System.out.println("API request to " + endpointLabel + " passed. Status code: " + statusCode);
+            } else {
+                System.out.println("API request to " + endpointLabel + " failed. Status code: " + statusCode);
+            }
+
+            Assert.assertEquals(statusCode, 200, "API request to " + endpointLabel + " failed");
+        }
+    }}
+
+
+
+
+
+
+
+
+
