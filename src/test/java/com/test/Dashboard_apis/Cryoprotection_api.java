273bee0fa9159c2e1443c690629574a79d2fdb56
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-15 15:12:35 +0530

    Cryoprotection_api.java

--- src/test/java/com/test/Dashboard_apis/Cryoprotection_api.java
+++ src/test/java/com/test/Dashboard_apis/Cryoprotection_api.java
@@ -5,146 +5,37 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class Cryoprotection_api {
 	
-	@Test(priority=1)
-	public void Cryoprotection()
-	{
-        Response response1 = RestAssured.get("https://apollo2.humanbrain.in/dashboard/dashboardCryoProtection");
-        int statusCode1 = response1.getStatusCode();
+	private static final Map<String, String> ENDPOINT_LABELS = new LinkedHashMap<>();
+    static {
+    	 ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/dashboardCryoProtection", "Cryo Protection");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/4", "Cryo_Protection_In");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/6", "Cryo_Protection_Waiting");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/7", "Cryo_Protection_Stage-I");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/8", "Cryo_protection_Stage_II");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/9", "Cryo_protection_Stage_III");
+        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/10", "Cryo_protection_Out");
         
-        Assert.assertEquals(statusCode1, 200, "API request to the Cryoprotection failed. Status code: " + statusCode1);
-        
-        System.out.println("API request to the Cryoprotection passed. Status code: " + statusCode1);
-	}
-	@Test(priority=2)
-    public void api_Cryo_IN()
-    
-       {
-           Response response1 = RestAssured
-                   .given()
-                   .auth()
-                   .basic("admin", "admin")
-                   .when()
-                   .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/4");
+    } 
 
-           int statusCode1 = response1.getStatusCode();
-           if (statusCode1 == 200) {
-           	System.out.println("_________________________******_______________________");
-               System.out.println("API request to the Dashboard Cryo_protection_IN passed. Status code: " + statusCode1);
-               System.out.println("_________________________******_______________________");
-           } else {
-           	System.out.println("_________________________******_______________________");
-               System.out.println("API request to the Dashboard Cryo_protection_IN failed. Status code: " + statusCode1);
-           }
-           Assert.assertEquals(statusCode1, 200, "API request to the Dashboard API request to the Dashboard Cryo_protection_IN failed");
-           
-       }
-	@Test(priority=3)
-	public void api_Waiting() {
-		 Response response1 = RestAssured
-                 .given()
-                 .auth()
-                 .basic("admin", "admin")
-                 .when()
-                 .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/6");
+    @Test
+    public void testAPIs() {
+        for (String endpoint : ENDPOINT_LABELS.keySet()) {
+            String endpointLabel = ENDPOINT_LABELS.get(endpoint);
+            Response response = RestAssured.get(endpoint);
 
-         int statusCode2 = response1.getStatusCode();
-         if (statusCode2 == 200) {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Waiting Passed");
-             System.out.println("_________________________******_______________________");
-         } else {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Waiting failed. Status code: " + statusCode2);
-         }
-         Assert.assertEquals(statusCode2, 200, "API request to the Dashboard API request to the Dashboard Cryo_protection_waiting failed");
-         
-     }
-	@Test(priority=4)
-	public void api_Stage_I() {
-		 Response response1 = RestAssured
-                .given()
-                .auth()
-                .basic("admin", "admin")
-                .when()
-                .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/7");
+            int statusCode = response.getStatusCode();
+            if (statusCode == 200) {
+                System.out.println("API request to " + endpointLabel + " passed. Status code: " + statusCode);
+            } else {
+                System.out.println("API request to " + endpointLabel + " failed. Status code: " + statusCode);
+            }
 
-        int statusCode2 = response1.getStatusCode();
-        if (statusCode2 == 200) {
-        	System.out.println("_________________________******_______________________");
-            System.out.println("API request to the Dashboard Cryo_protection_Stage_I passed. Status code: " + statusCode2);
-            System.out.println("_________________________******_______________________");
-        } else {
-        	System.out.println("_________________________******_______________________");
-            System.out.println("API request to the Dashboard Cryo_protection_Stage_I  failed. Status code: " + statusCode2);
+            Assert.assertEquals(statusCode, 200, "API request to " + endpointLabel + " failed");
         }
-        Assert.assertEquals(statusCode2, 200, "API request to the Dashboard Cryo_protection_Stage_I failed");
-        
-    }
-		
-	@Test(priority=5)
-	public void api_Stage_II() {
-		 Response response1 = RestAssured
-                 .given()
-                 .auth()
-                 .basic("admin", "admin")
-                 .when()
-                 .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/8");
-
-         int statusCode2 = response1.getStatusCode();
-         if (statusCode2 == 200) {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Stage_II passed. Status code: " + statusCode2);
-             System.out.println("_________________________******_______________________");
-         } else {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Stage_II  failed. Status code: " + statusCode2);
-         }
-         Assert.assertEquals(statusCode2, 200, "API request to the Dashboard Cryo_protection_Stage_II failed");
-         
-     }
-	@Test(priority=6)
-	public void api_Stage_III() {
-		 Response response1 = RestAssured
-                 .given()
-                 .auth()
-                 .basic("admin", "admin")
-                 .when()
-                 .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/9");
-
-         int statusCode2 = response1.getStatusCode();
-         if (statusCode2 == 200) {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Stage_III passed. Status code: " + statusCode2);
-             System.out.println("_________________________******_______________________");
-         } else {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Stage_III  failed. Status code: " + statusCode2);
-         }
-         Assert.assertEquals(statusCode2, 200, "API request to the Dashboard Cryo_protection_Stage_III failed");
-         
-     }
-	@Test(priority=7)
-	public void api_Cryo_Out() {
-		 Response response1 = RestAssured
-                 .given()
-                 .auth()
-                 .basic("admin", "admin")
-                 .when()
-                 .get("https://apollo2.humanbrain.in/dashboard/queryOrganStatus/10");
-
-         int statusCode2 = response1.getStatusCode();
-         if (statusCode2 == 200) {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Out passed. Status code: " + statusCode2);
-             System.out.println("_________________________******_______________________");
-         } else {
-         	System.out.println("_________________________******_______________________");
-             System.out.println("API request to the Dashboard Cryo_protection_Out  failed. Status code: " + statusCode2);
-         }
-         Assert.assertEquals(statusCode2, 200, "API request to the Dashboard Cryo_protection_Out failed");
-     }
-
-}
+    }}
