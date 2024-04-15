35fe7547b8606c6760e016fbdcc37add659bbf24
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-15 15:14:30 +0530

    MRI_api.java

--- src/test/java/com/test/Dashboard_apis/MRI_api.java
+++ src/test/java/com/test/Dashboard_apis/MRI_api.java
@@ -5,18 +5,40 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MRI_api {
-	
-	@Test(priority=1)
-	public void MRI()
-	{
-        Response response1 = RestAssured.get("https://apollo2.humanbrain.in/dashboard/dashboardMRI");
-        int statusCode1 = response1.getStatusCode();
-        
-        Assert.assertEquals(statusCode1, 200, "API request to the MRI failed. Status code: " + statusCode1);
-        
-        System.out.println("API request to the MRI passed. Status code: " + statusCode1);
-	}
+	private static final Map<String, String> ENDPOINT_LABELS = new LinkedHashMap<>();
+	    static {
+	    	 ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/dashboardMRI", "MRI_IN");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/1", "MRI_Upload");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/2", "MRI_Automatic");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/3", "MRI_ManualCorrect");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/4", "MRI_QC");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/5", "MRI_Redo");
+	        ENDPOINT_LABELS.put("https://apollo2.humanbrain.in/dashboard/querySkullStrip/6", "MRI_Out");
 
-}
+	    } 
+
+	    @Test
+	    public void testAPIs() {
+	        for (String endpoint : ENDPOINT_LABELS.keySet()) {
+	            String endpointLabel = ENDPOINT_LABELS.get(endpoint);
+	            Response response = RestAssured.get(endpoint);
+
+	            int statusCode = response.getStatusCode();
+	            if (statusCode == 200) {
+	                System.out.println("API request to " + endpointLabel + " passed. Status code: " + statusCode);
+	            } else {
+	                System.out.println("API request to " + endpointLabel + " failed. Status code: " + statusCode);
+	            }
+
+	            Assert.assertEquals(statusCode, 200, "API request to " + endpointLabel + " failed");
+	        
+	        }}}
+
+
+
+	
+	
